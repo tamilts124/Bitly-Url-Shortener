cd1a818eead98f5f848b5b5fc71756986922f017
Tamil Arasan <74599391+tamilts124@users.noreply.github.com>
2022-09-21 18:59:24 +0300

    Updated!
    Final Commit

--- UrlShortener.py
+++ UrlShortener.py
@@ -1,4 +1,4 @@
-import cloudscraper, optparse, json, sys
+import cloudscraper, optparse, json
 
 parser =optparse.OptionParser()
 scraper =cloudscraper.create_scraper(browser={'browser': 'firefox','platform': 'windows','mobile': False})
@@ -15,11 +15,9 @@ def find_all():
     else:return False
 
 def request_resourse(url, data={}, headers={}, method='GET', cookies={}, allow_redirects=True):
-    try:
-        if method.upper()=='POST':response =scraper.post(url, data=data, headers=headers, cookies=cookies, allow_redirects=allow_redirects)
-        else:response =scraper.get(url, data=data, headers=headers, cookies=cookies, allow_redirects=allow_redirects)
-        return response
-    except Exception as e:print(e);sys.exit(1)
+    if method.upper()=='POST':response =scraper.post(url, data=data, headers=headers, cookies=cookies, allow_redirects=allow_redirects)
+    else:response =scraper.get(url, data=data, headers=headers, cookies=cookies, allow_redirects=allow_redirects)
+    return response
 
 def free_url_shortener(longurl):
     _xsrf =request_resourse('https://bitly.com/').cookies['_xsrf']
@@ -28,45 +26,36 @@ def free_url_shortener(longurl):
         try:result =result.json()
         except Exception:continue
         else:break
-    if result['status_txt']=="OK":print(f"    Link: {result['data']['link']}");Thank()
-    elif result['status_txt']=="RATE_LIMIT_EXCEEDED":print("    MAXIMUN LIMIT REACHED.., TRY AGAIN LATER (or) USE USER ACCOUNT!");Thank()
-    elif result['status_txt']=="INVALID_ARG_URL":print("    INVALID URL, PROPARE URL NEED FOR SHORT!");Thank()
-    else: print(f"    {result['status_txt']}!");Thank()
+    if result['status_txt']=="OK":print(f" Link: {result['data']['link']}")
+    elif result['status_txt']=="RATE_LIMIT_EXCEEDED":print("> MAXIMUN LIMIT REACHED.., TRY AGAIN LATER (or) USE USER ACCOUNT!")
+    elif result['status_txt']=="INVALID_ARG_URL":print("> INVALID URL, PROPARE URL NEED FOR SHORT!")
+    else: print(f"> {result['status_txt']}!")
 
 def user_url_shortener(longurl, user, password):
     _xsrf =request_resourse('https://bitly.com/').cookies['_xsrf']
     user_account =request_resourse('https://bitly.com/a/sign_in', {'rd':r'/', 'username':user, 'password':password, '_xsrf':_xsrf, 'verificaton':True}, {'X-Xsrftoken':_xsrf, 'Content-Type':'application/x-www-form-urlencoded'}, 'POST', {'_xsrf':_xsrf}, False)
     user =user_account.cookies.get('user', False)
-    if not user:print('    USER ACCOUNT NOT FOUND!');return
+    if not user:print('> USER ACCOUNT NOT FOUND!');return
     result =request_resourse('https://app.bitly.com/bbt2/', {}, {}, 'GET', {'_xsrf':_xsrf, 'user':user});result =result.text
     group_guid =result.split('groups: ["')[1].split('"')[0]
     json_data =json.dumps({'long_url':longurl, 'group_guid':group_guid})
     result =request_resourse('https://app.bitly.com/proxy/v4/shorten', json_data, {'X-Xsrftoken':_xsrf, 'X-Bitly-Client':'bbt2', 'Referer': 'https://app.bitly.com', 'Content-Type':'application/json'}, 'POST', {'_xsrf':_xsrf, 'user':user})
     result =result.json()
-    if result.get('link', False):print(f"    Link: {result['link']}");Thank()
+    if result.get('link', False):print(f"    Link: {result['link']}")
     elif result.get('status_text', False):
-        if result['status_text']=="INVALID_ARG_LONG_URL":print("    INVALID URL, PROPARE URL NEED FOR SHORT!");Thank()
-        elif result['status_text']=="RATE_LIMIT_EXCEEDED":print("    MAXIMUN LIMIT REACHED.., TRY AGAIN LATER (or) USE USER ACCOUNT!");Thank()
-        else: print(f"    {result['status_text']}!");Thank()
-    else: print('    SHORT URL CAN\'T RECEIVE!');Thank()
-
-def Thank():
-    print();print()
-    print('    /////////////////////////////////////////////////////')
-    print('    /////// For More Follow In Github @tamilts124 ///////')
-    print('    /////////////////////////////////////////////////////')
+        if result['status_text']=="INVALID_ARG_LONG_URL":print("> INVALID URL, PROPARE URL NEED FOR SHORT!")
+        elif result['status_text']=="RATE_LIMIT_EXCEEDED":print("> MAXIMUN LIMIT REACHED.., TRY AGAIN LATER (or) USE USER ACCOUNT!")
+        else: print(f"> {result['status_text']}!")
+    else: print('> SHORT URL CAN\'T RECEIVE!')
 
 def Main():
     global option
-    try:
-        option =parser.parse_args()[0]
-        find =find_all()
-        if not find:parser.print_help();return
-        print()
-        if find==1:user_url_shortener(option.longurl, option.account, option.password)
-        elif find==2:free_url_shortener(option.longurl)
-    except KeyboardInterrupt:Thank()
+    option =parser.parse_args()[0]
+    find =find_all()
+    if not find:parser.print_help();return
     print()
+    if find==1:user_url_shortener(option.longurl, option.account, option.password)
+    elif find==2:free_url_shortener(option.longurl)
 
 if __name__ == '__main__':
     help();Main()
